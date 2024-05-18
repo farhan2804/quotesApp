677ff7a9f9bdd3b7a575f00cc930d2548755fd92
farhan <el3317.farhanmahmood@gmail.com>
2024-05-18 17:43:29 +0530

    updated the quotes

--- src/quotesData.js
+++ src/quotesData.js
@@ -5,11 +5,11 @@ import IMAGE4 from "./assets/Images/4.jpg";
 import IMAGE5 from "./assets/Images/5.jpg";
 
 const quotesData = [
-  "Jab aap kisi Cheez se Dur Bhaagte Ho, toh ye Duniya bhi choti lagne lagti hai",
-  "Insaan ko apna ghum sirf tab tak bara lagta hain, jab tak wo dusron ka ghum na dekh le",
-  "Your time is limited, so don't waste it living someone else's life. - Steve Jobs",
-  "The way to get started is to quit talking and begin doing. - Walt Disney",
-  "Your time is limited, so don't waste it living someone else's life. - Steve Jobs",
+  "Jab aap kisi Cheez se Dur Bhaagte Ho, toh ye Duniya bhi chhoti lagne lagti hai.....",
+  "Insaan ko apna ghum sirf tab tak bara lagta hain, jab tak wo dusron ka ghum na dekh le.....",
+  "Sabr karna mushkil tab hota hain, jab aazmaaishey lambi ho jaayein.....",
+  "To win any battle, first, you need to stay on the battlefield, otherwise, you are already out of it.....",
+  "Aap kamzor nahi hai, shaayad duniya hi kuch zyaada zaalim hain.....",
 ];
 const quotesImages = [IMAGE1, IMAGE2, IMAGE3, IMAGE4, IMAGE5];
 export { quotesData, quotesImages };
