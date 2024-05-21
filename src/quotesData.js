5886657e59ea213472cc37a792b907bd573fc036
farhan <el3317.farhanmahmood@gmail.com>
2024-05-21 10:53:48 +0530

    added new quotes

--- src/quotesData.js
+++ src/quotesData.js
@@ -3,13 +3,15 @@ import IMAGE2 from "./assets/Images/2.jpg";
 import IMAGE3 from "./assets/Images/3.jpg";
 import IMAGE4 from "./assets/Images/4.jpg";
 import IMAGE5 from "./assets/Images/5.jpg";
+import IMAGE6 from "./assets/Images/6.jpg";
 
 const quotesData = [
-  "Jab aap kisi Cheez se Dur Bhaagte Ho, toh ye Duniya bhi chhoti lagne lagti hai.....",
-  "Insaan ko apna ghum sirf tab tak bara lagta hain, jab tak wo dusron ka ghum na dekh le.....",
-  "Sabr karna mushkil tab ho jaata hain, jab aazmaaishey lambi ho jaayein.....",
-  "To win any battle, first, you need to stay on the battlefield, otherwise, you are already out of it.....",
-  "Aap kamzor nahi hai, shaayad duniya hi kuch zyaada zaalim hain.....",
+  "Jab aap kisi Cheez se Dur Bhaagte Ho, toh ye Duniya bhi chhoti lagne lagti hai",
+  "Insaan ko apna ghum sirf tab tak bara lagta hain, jab tak wo dusron ka ghum na dekh le",
+  "Sabr karna mushkil tab ho jaata hain, jab aazmaaishey lambi ho jaayein",
+  "To win any battle, first, you need to stay on the battlefield, otherwise, you are already out of it",
+  "Aap kamzor nahi hai, shaayad duniya hi kuch zyaada zaalim hain",
+  "Tumhe to khush hona chahye,yaad karo tm hi to kaha karte the ' ALLAH mujhe sirf tera mohtaaj banana '"
 ];
-const quotesImages = [IMAGE1, IMAGE2, IMAGE3, IMAGE4, IMAGE5];
+const quotesImages = [IMAGE1, IMAGE2, IMAGE3, IMAGE4, IMAGE5,IMAGE6];
 export { quotesData, quotesImages };
