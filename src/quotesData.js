ed7c154a65f7275a3b9f89cc970d426b8ea82b1b
farhan <el3317.farhanmahmood@gmail.com>
2024-05-28 10:50:36 +0530

    added new quote

--- src/quotesData.js
+++ src/quotesData.js
@@ -5,15 +5,17 @@ import IMAGE4 from "./assets/Images/4.jpg";
 import IMAGE5 from "./assets/Images/5.jpg";
 import IMAGE6 from "./assets/Images/6.jpg";
 import IMAGE7 from "./assets/Images/8.jpg";
+import IMAGE8 from "./assets/Images/7.jpg";
 
 const quotesData = [
   "Jab aap kisi Cheez se Dur Bhaagte Ho, toh ye Duniya bhi chhoti lagne lagti hai",
   "Insaan ko apna ghum sirf tab tak bara lagta hain, jab tak wo dusron ka ghum na dekh le",
   "Sabr karna mushkil tab ho jaata hain, jab aazmaaishey lambi ho jaayein",
   "To win any battle, first, you need to stay on the battlefield, otherwise, you are already out of it",
-  "Aap kamzor nahi hai, shaayad duniya hi kuch zyaada zaalim hain",
+  "Aap kamzor nahi hai, shaayad duniya hi kuch zyaada zaalim hai",
   "Tumhe to khush hona chahye,yaad karo tm hi to kaha karte the,ALLAH mujhe sirf tera mohtaaj banana",
-  "Jab aazmaaishey barh jaaye na to samajh lena,tumhaari manzil bahout kareeb hai"
+  "Jab aazmaaishey barh jaaye na to samajh lena,tumhaari manzil bahout kareeb hai",
+  "Zindagi me kuch bhi 'yuhi' nahi hota, har cheez ka apna ek maqsad hota hai",
 ];
-const quotesImages = [IMAGE1, IMAGE2, IMAGE3, IMAGE4, IMAGE5, IMAGE6,IMAGE7];
+const quotesImages = [IMAGE1, IMAGE2, IMAGE3, IMAGE4, IMAGE5, IMAGE6, IMAGE7,IMAGE8];
 export { quotesData, quotesImages };
