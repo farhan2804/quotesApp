089a9b357cd71a877d8ba1c1c21483b0f0480af3
farhan <el3317.farhanmahmood@gmail.com>
2024-05-29 18:00:12 +0530

    added new background

--- src/quotesData.js
+++ src/quotesData.js
@@ -4,8 +4,9 @@ import IMAGE3 from "./assets/Images/3.jpg";
 import IMAGE4 from "./assets/Images/4.jpg";
 import IMAGE5 from "./assets/Images/5.jpg";
 import IMAGE6 from "./assets/Images/6.jpg";
-import IMAGE7 from "./assets/Images/8.jpg";
-import IMAGE8 from "./assets/Images/7.jpg";
+import IMAGE7 from "./assets/Images/7.jpg";
+import IMAGE8 from "./assets/Images/8.jpg";
+import IMAGE9 from './assets/Images/9.jpg';
 
 const quotesData = [
   "Jab aap kisi Cheez se Dur Bhaagte Ho, toh ye Duniya bhi chhoti lagne lagti hai",
@@ -17,5 +18,5 @@ const quotesData = [
   "Jab aazmaaishey barh jaaye na to samajh lena,tumhaari manzil bahout kareeb hai",
   "Zindagi me kuch bhi 'yuhin' nahi hota, har cheez ka apna ek maqsad hota hai",
 ];
-const quotesImages = [IMAGE1, IMAGE2, IMAGE3, IMAGE4, IMAGE5, IMAGE6, IMAGE7,IMAGE8];
+const quotesImages = [IMAGE1, IMAGE2, IMAGE3, IMAGE4, IMAGE5, IMAGE6, IMAGE7,IMAGE9];
 export { quotesData, quotesImages };
