62e487020f577219238f97138aca51f72f6d5b88
farhan <el3317.farhanmahmood@gmail.com>
2024-05-29 09:50:39 +0530

    added new quote

--- src/quotesData.js
+++ src/quotesData.js
@@ -15,7 +15,7 @@ const quotesData = [
   "Aap kamzor nahi hai, shaayad duniya hi kuch zyaada zaalim hai",
   "Tumhe to khush hona chahye,yaad karo tm hi to kaha karte the,ALLAH mujhe sirf tera mohtaaj banana",
   "Jab aazmaaishey barh jaaye na to samajh lena,tumhaari manzil bahout kareeb hai",
-  "Zindagi me kuch bhi 'yuhi' nahi hota, har cheez ka apna ek maqsad hota hai",
+  "Zindagi me kuch bhi 'yuhin' nahi hota, har cheez ka apna ek maqsad hota hai",
 ];
 const quotesImages = [IMAGE1, IMAGE2, IMAGE3, IMAGE4, IMAGE5, IMAGE6, IMAGE7,IMAGE8];
 export { quotesData, quotesImages };
